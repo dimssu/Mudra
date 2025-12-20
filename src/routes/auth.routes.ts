7655d438bd1731e8927188a8614939d2676d4b3c
dimssu <aryansi1126@gmail.com>
2025-12-20 06:42:57 +0530

    feat: Add Firebase Admin SDK key and implement refresh token validation on the refresh-token route.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller';
 import { authenticate } from '../middlewares/auth.middleware';
+import { validateRefreshToken } from '../middlewares/validation.middleware';
 
 const router = Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
-router.post('/refresh-token', AuthController.refreshToken);
+router.post('/refresh-token', validateRefreshToken, AuthController.refreshToken);
 router.post('/logout', authenticate, AuthController.logout);
 router.post('/google/get-access', AuthController.getAccessWithGoogle)
 router.post('/verify-token', AuthController.verifyToken)
